ff1394be0f74b66a730547c4627b5d5669d0bccc
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-06-29 16:19:58 -0500

    Basics

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,37 @@
+import MovieCard from "../components/MovieCard"
+import { useState } from "react"
+
+function Home() {
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const movies = [
+        { id: 1, title: "MIB", release_date: "2002" },
+        { id: 2, title: "Avengers", release_date: "2003" },
+        { id: 3, title: "International", release_date: "2004" },
+    ]
+
+    const handleSearch = (e) => {
+        e.preventDefault()
+        alert(searchQuery)
+        setSearchQuery("")
+    };
+
+    return <div className="home">
+        <form onSubmit={handleSearch} className="search-form">
+            <input type="text"
+            className="search-input"
+            placeholder="Search for movies...."
+            value={searchQuery} 
+            onChange={(e) =>setSearchQuery(e.target.value)}/>
+            <button type="submit" className="search-button">Search</button>
+        </form>
+
+        <div className="movies-grid">
+            {movies.map((movie => 
+                movie.title.toLowerCase().startsWith(searchQuery) && (
+                <MovieCard movie={movie} key={movie.id} />
+                )))}
+        </div>
+    </div>
+}
+export default Home
