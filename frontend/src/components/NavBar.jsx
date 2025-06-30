e024a3993e5885f695be663f3acdc3c8e541b229
rwickramasinghe06 <rwickramasinghe@academic.rrc.ca>
2025-06-30 09:47:42 -0500

    Fixed NavBar

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -7,6 +7,7 @@ function NavBar(params) {
             <Link to="/">Movie App</Link>
         </div>
         <div className="navbar-links">
+            <Link to="/" className="nav-link">Home</Link>
             <Link to="/favorites" className="nav-link">Favorites</Link>
         </div>
     </nav>
